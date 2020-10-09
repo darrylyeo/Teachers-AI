58c4d34080dc0af933c458cbd79d428e59073b56
Sanjana Gundala <gundala@calpoly.edu>
2020-10-09 14:32:42 -0700

    Update tai.py
    added 2 and 3 requirements

--- tai.py
+++ tai.py
@@ -1,5 +1,6 @@
 import json, nltk, re, sys
 from nltk.tokenize import PunktSentenceTokenizer, sent_tokenize
+from nltk.corpus import wordnet as wn
 
 
 checkboxWeights = {
@@ -68,17 +69,46 @@ def scoreLead(text):
 
 	return gradeLevelToScore(grade)
  
-def scoreTransitions(text):
-	words = nltk.word_tokenize(text)
+def scoreTransitions(text, lead, body, ending):
+	all_words = nltk.word_tokenize(text)
+	print(all_words)
 	sentences = nltk.sent_tokenize(text)
 
 	grade = 0
-	if True:
+
+	#include synsets of such as, and, also 
+	synonyms =["and", "also"]
+	synsets = []
+	synsets.extend(wn.synsets("and"))
+	synsets.extend(wn.synsets("also"))
+	for syn in synsets:
+		for l in syn.lemmas():
+			synonyms.append(l.name().replace("_"," "))
+	if len(set(all_words).intersection(synonyms)) > 0:
 		grade += checkboxWeights['2-0_transitions']
-	if True:
+	#include synsets of before, after, then, later
+	synonyms =["before","after","then","later"]
+	synsets = []
+	synsets.extend(wn.synsets("before"))
+	synsets.extend(wn.synsets("after"))
+	synsets.extend(wn.synsets("then"))
+	synsets.extend(wn.synsets("later"))
+	for syn in synsets:
+		for l in syn.lemmas():
+			synonyms.append(l.name().replace("_"," "))
+	if len(set(all_words).intersection(synonyms)) > 1:
 		grade += checkboxWeights['3-0_transitions']
-	if True:
+	#include synsets of however and but
+	synonyms =["however", "but"]
+	synsets = []
+	synsets.extend(wn.synsets("however"))
+	synsets.extend(wn.synsets("but"))
+	for syn in synsets:
+		for l in syn.lemmas():
+			synonyms.append(l.name().replace("_"," "))
+	if len(set(all_words).intersection(synonyms)) > 0:
 		grade += checkboxWeights['3-1_transitions']
+	'''
 	if True:
 		grade += checkboxWeights['4-0_transitions']
 	if True:
@@ -93,8 +123,8 @@ def scoreTransitions(text):
 		grade += checkboxWeights['5-2_transitions']
 	if True:
 		grade += checkboxWeights['5-3_transitions']
-
-
+	'''
+	print(grade)
 	return gradeLevelToScore(grade)
 
 def scoreEnding(text):
@@ -143,7 +173,7 @@ def scoreEssay(text):
 
 
 	print('Sections:', sections, '\n')
-
+	
 	if len(sections) < 3:
 		print('No clear lead, body, or ending')
 	else:
@@ -157,10 +187,10 @@ def scoreEssay(text):
 		# print(body)
 		# print('Ending:')
 		# print(ending)
-
-		print('Lead:', scoreLead(lead))
-		print('Transitions:', scoreTransitions(body))
-		print('Ending:', scoreEnding(ending))
+	
+		#print('Lead:', scoreLead(lead))
+		print('Transitions:', scoreTransitions(text, lead, body, ending))
+		#print('Ending:', scoreEnding(ending))
 
 
 
